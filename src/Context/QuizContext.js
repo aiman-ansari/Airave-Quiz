bc9cce2929da2e139af85035e1b76c65a10e2706
Aiman <ansariaiman2000@gmail.com>
2022-06-20 02:27:47 -0700

    feat: Added Quiz page

--- src/Context/QuizContext.js
+++ src/Context/QuizContext.js
@@ -1,4 +1,11 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useReducer,
+} from "react";
+import { quizReducer } from "../Reducer/quizReducer";
 import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 import { db } from "../firebase";
 const QuizContext = createContext();
@@ -11,17 +18,40 @@ const QuizProvider = ({ children }) => {
   useEffect(() => {
     getQuizes();
   }, []);
-
+  const [currentQuiz, setCurrentQuiz] = useState({
+    getAll: [],
+    score: 0,
+  });
   const getAllQuizes = () => {
     return getDocs(quizes);
   };
+  const handleQuiz = (correct, item) => {
+    setCurrentQuiz((prev) => ({
+      ...prev,
+      getAll: [...prev.getAll, item],
+    }));
 
+    if (correct === item) {
+      setCurrentQuiz((prev) => ({
+        ...prev,
+        score: prev.score + 5,
+      }));
+    }
+  };
   const getQuizes = async () => {
     const data = await getAllQuizes();
     setQuiz(data.docs.map((doc) => ({ ...doc.data() })));
   };
+  const [state, dispatch] = useReducer(quizReducer, {
+    active: "",
+  });
+  console.log(currentQuiz);
   return (
-    <QuizContext.Provider value={{ quiz }}>{children}</QuizContext.Provider>
+    <QuizContext.Provider
+      value={{ quiz, currentQuiz, handleQuiz, state, dispatch }}
+    >
+      {children}
+    </QuizContext.Provider>
   );
 };
 
